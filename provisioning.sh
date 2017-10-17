e5031b15da64033faa7818d44edb87280a75ecae
Nao Minami <south37777@gmail.com>
2017-10-17 14:18:27 +0900

    Add provisining.sh

---
+++ provisioning.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo "Provisioning Start..."
+
+cd $HOME
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y --no-install-recommends ansible git aptitude golang-go tzdata make
+rm -rf isucon6-qualify
+git clone https://github.com/isucon/isucon6-qualify.git
+sed -i -e 's:--disable-phar::' isucon6-qualify/provisioning/image/ansible/02_xbuild.yml
+(
+  cd isucon6-qualify
+  make
+  ln -s isupam_linux bin/isupam
+)
+(
+  cd isucon6-qualify/provisioning/image/ansible
+  PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ansible-playbook -i localhost, *.yml --connection=local -t dev
+)
+(
+  cd isucon6-qualify/provisioning/image
+  ./db_setup.sh
+)
+rm -rf isucon6-qualify
+usermod -G sudo -a -s /bin/bash isucon
+
+echo "Provisioning Successful for image"
